6387a1c1eadffb0b2c9620cd31dcbd8d97f9119e
kelley·胡 <40583480+cappuccino5@users.noreply.github.com>
2020-05-26 00:30:27 +0800

    ..
    ...

--- base_test.go
+++ base_test.go
@@ -164,3 +164,156 @@ func quickSort(front, rear int, array []int) {
 		quickSort(front, right, array)
 	}
 }
+
+
+
+// 测试听胡
+func TestTingHu(t *testing.T) {
+	var userMj = map[int32]map[int32]int32{0: {
+		0x01: 3, 0x03: 3, 0x06: 1, 0x07: 1, 0x08: 1, 0x15: 1,0x16: 1,
+	},
+		1: {
+			0x01: 3, 0x03: 3, 0x06: 1, 0x07: 1, 0x08: 1, 0x18: 1,0x19: 1,
+		},
+		2: {0x01: 2, 0x03: 3, 0x06: 1, 0x07: 1, 0x08: 1,0x17:1, 0x18: 1,0x19: 1,}}
+
+	respCode := GetOutMjData(userMj[2])
+	fmt.Printf("code :%v,hex:%X\n", respCode, respCode)
+}
+
+// 获取最佳出牌数据，出牌后缺的听胡数最少
+func GetOutMjData(userMjData map[int32]int32) map[int32][]int32 {
+	replaceCard := func(card int32, tempCards map[int32]int32) map[int32]int32 {
+		if tempCards[card] != 0 {
+			tempCards[card] --
+		}
+		tempCards[0x35]++
+		return tempCards
+	}
+	MagicMahjong := userMjData[global.MagicMahjong] //记录癞子
+	var OutCardsAsc = make(map[int32]int32, 0)
+	var array []int32
+	var tingHuCards = make(map[int32][]int32, 0)
+	for k, num := range userMjData {
+		if num <= 0 {
+			delete(userMjData, k)
+			continue
+		}
+		if k != 0x35 {
+			queCount := GetMininum(replaceCard(k, userMjData))
+			if queCount == 1 {
+				OutCardsAsc[k] = num
+				array = append(array, k)
+			}
+			userMjData[k]++
+			userMjData[global.MagicMahjong] = MagicMahjong
+			if MagicMahjong == 0 {
+				delete(userMjData, global.MagicMahjong)
+			}
+		}
+	}
+	for i := 0; i < len(array)-1; i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] > array[j] {
+				array[j], array[i] = array[i], array[j]
+			}
+		}
+	}
+	fmt.Println("array:", array)
+	for k, num := range OutCardsAsc {
+		for i := 1; i < len(array); i++ {
+			if array[i] == array[i-1]+1 && num >= 2 {
+				if array[i-1]&0x0F <= 7 {
+					tingHuCards[k] = append(tingHuCards[k], array[i]+1, array[i-1]-1)
+				} else if array[i]&0x0F > 7 {
+					tingHuCards[k] = append(tingHuCards[k], array[i-1]-1)
+				}
+			} else if array[i] != k && num == 1 && OutCardsAsc[array[i]] == 1 {
+				tingHuCards[k] = append(tingHuCards[k], array[i])
+			}
+		}
+	}
+	return tingHuCards
+}
+
+//获取最低需要癞子数
+func GetMininum(userMahjong map[int32]int32) int32 {
+	var data [31]int32
+	for k, v := range userMahjong {
+		if k == global.MagicMahjong {
+			continue
+		}
+
+		data[SwitchToIndex(k)] = v
+	}
+
+	var Minimum int32
+	for k, v := range data {
+		var queCount int32
+		//取出一对将
+		tempData := data
+		switch v {
+		case 0:
+			queCount += 2
+		case 1:
+			queCount++
+			tempData[k] -= 1
+		case 2:
+			tempData[k] -= 2
+		case 3:
+			tempData[k] -= 2
+		case 4:
+			tempData[k] -= 2
+		}
+
+		for i := 0; i < 31; {
+			if tempData[i] == 0 {
+				i++
+				continue
+			}
+
+			//数值
+			value := i%9 + 1
+			for tempData[i] > 0 {
+				//判断是否是顺子
+				if value <= 0x09-2 && tempData[i+1] > 0 && tempData[i+2] > 0 {
+					for j := 0; j < 3; j++ {
+						tempData[i+j]--
+					}
+					continue
+				}
+
+				switch tempData[i] {
+				case 1:
+					if value <= 0x08 && ((value == 0x08 && tempData[i+1] > 0) || tempData[i+1] > 0 || tempData[i+2] > 0) {
+						if tempData[i+1] != 0 {
+							tempData[i+1] --
+						}
+						if value != 0x08 && tempData[i+2] != 0 {
+							tempData[i+2]--
+						}
+						queCount++
+					} else {
+						queCount += 2
+					}
+				case 2:
+					queCount++
+				case 3:
+				case 4:
+					queCount += 2
+				}
+
+				tempData[i] = 0
+			}
+		}
+
+		if Minimum == 0 && queCount != 0 {
+			Minimum = queCount
+		}
+		if queCount < Minimum {
+			Minimum = queCount
+		}
+	}
+
+	return Minimum
+}
